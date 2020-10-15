f6886a195f6e97c4271e9941a28a118d7e4450ff
mki <mail@kissel.ch>
2020-10-15 12:05:00 +0200

    add day_2

---
+++ day_2/variables.py
@@ -0,0 +1,18 @@
+# Day 2: 30 Days of python programming
+
+first_name = "mik"
+last_name = "ch06"
+fullname = "mik ch06"
+country = "Switzerland"
+city = "Zug"
+age = 39
+year = 2020
+is_married = True
+is_light_on = True
+
+a, b = "mik", "ch06"
+
+print(type("mik"))
+
+print("Show length of First name: ")
+print(len(first_name))
